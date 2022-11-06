ebcdbbc466f2ac4c980e33c5d1414a6e5fdaee55
meequrox <117393211+meequrox@users.noreply.github.com>
2022-11-06 16:55:37 +0700

    Implement get_paths() function
    get_paths() returns a vector with file paths in the current working directory
    
    Example:
    $ pwd
    	/home/user/Videos
    $ ls
    	1.mp4  2.mp4  3.mp4  RandomVideoOpener
    $ ./RandomVideoOpener
    	/home/user/Videos:
    	/home/user/Videos/RandomVideoOpener
    	/home/user/Videos/2.mp4
    	/home/user/Videos/3.mp4
    	/home/user/Videos/1.mp4

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,38 @@
+//#include <iostream>
+
+#include <vector>
+#include <filesystem>
+#include <iostream>
+
+#define PRINT
+
+namespace fs = std::filesystem;
+
+std::vector<fs::path> get_paths() {
+    fs::path dir = fs::current_path();
+    std::vector<fs::path> files;
+
+    for (const auto &entry : fs::directory_iterator(dir)) {
+        // TODO: Format check
+        if (entry.is_regular_file())
+            files.push_back(entry);
+    }
+
+#ifdef PRINT
+    std::cout << dir.generic_string() << ":" << std::endl;
+#endif
+
+    return files;
+}
+
 int main() {
+
+    std::vector<fs::path> files = get_paths();
+
+#ifdef PRINT
+    for (int i = 0; i < files.size(); i++)
+        std::cout << files.at(i).generic_string() << std::endl;
+#endif
+
 	return 0;
 }
