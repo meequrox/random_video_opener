702ae828b03d81f2bac655faf7761423c437ccd9
meequrox <117393211+meequrox@users.noreply.github.com>
2022-11-28 21:03:57 +0700

    Use directory class in main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,12 @@
 #include <iostream>
-#include <random>
 
-#include "../thirdparty/pcg_random.hpp"
-#include "Colors/colors.hpp"
-#include "files.hpp"
+#include "directory.hpp"
 
 int main() {
-    // idk how it works, but it repairs coloring in Windows cmd & PowerShell
-#ifdef COLOR_TARGET_WINDOWS
-    system("");
-#endif
+    mqr::directory d;
 
-    std::vector<fs::path> files = get_files_in_dir(fs::current_path());
-
-    if (!files.size()) {
-        std::cout << Color::Red << "No files found, exiting..." << Color::Standard << std::endl;
-        return EXIT_SUCCESS;
-    }
-
-    pcg32_fast rng(pcg_extras::seed_seq_from<std::random_device>{});
-
-    // [0; files.size()]
-    int rand = rng(files.size());
-
-    print_info(fs::current_path(), files, rand);
-
-    open_file_at_index(fs::current_path(), files, rand);
-
-    std::cout << Color::Green << "You can now close this window" << Color::Standard << std::endl;
+    d.printInfo();
+    d.openRandomFile();
 
     return EXIT_SUCCESS;
 }
